abe1c5002e82dce825d01a86109786b5eed1272b
vanshmehandru <vanshxzaddy@gmail.com>
2026-03-25 14:34:12 +0530

    UC4 - Maintain Ordered Bogie IDs (TreeSet & SortedSet)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,32 +1,35 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
 
-public class TrainApp {
+public class TrainConsistManagementApp {
     public static void main(String[] args) {
 
         System.out.println("==================================");
-        System.out.println("UC3 - Track Unique Bogie IDs ");
+        System.out.println("UC4 - Maintain Ordered Train Consist");
         System.out.println("==================================\n");
 
-        // Create a Set to store unique bogie IDs
-        Set<String> bogies = new HashSet<>();
+        // Create LinkedList to maintain order
+        LinkedList<String> train = new LinkedList<>();
 
-        // Add IDs (including duplicates)
-        bogies.add("BG101");
-        bogies.add("BG102");
-        bogies.add("BG103");
-        bogies.add("BG104");
+        // Add bogies
+        train.add("Engine");
+        train.add("Sleeper");
+        train.add("AC Chair");
+        train.add("Cargo");
+        train.add("Guard");
 
-        // Duplicate entries (ignored automatically)
-        bogies.add("BG101");
-        bogies.add("BG102");
+        System.out.println("Initial Train Consist:");
+        System.out.println(train);
 
-        System.out.println("Bogie IDs After Insertion:");
-        System.out.println(bogies);
+        // Insert Pantry Car at position 2
+        train.add(2, "Pantry Car");
 
-        System.out.println("\nNote:");
-        System.out.println("Duplicates are automatically ignored by HashSet.");
+        // Remove first and last bogie
+        train.removeFirst();
+        train.removeLast();
 
-        System.out.println("\nUC3 uniqueness validation completed...");
+        System.out.println("\nFinal Train Consist:");
+        System.out.println(train);
+
+        System.out.println("\nUC4 ordering and modification completed...");
     }
 }
